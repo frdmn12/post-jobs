c77e162c5c03973e2a3cb86dba637f442ec9569a
bayu <bayuferdiman1@gmail.com>
2024-08-02 10:53:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// Font SF Pro Display
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
